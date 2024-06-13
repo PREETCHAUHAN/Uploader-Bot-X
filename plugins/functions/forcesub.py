3b63d8d3d96fedc116e3b6b208649aff9be27c07
DJ Preet <33035404+PREETCHAUHAN@users.noreply.github.com>
2024-06-13 23:36:22 +0530

    Update forcesub.py

--- plugins/functions/forcesub.py
+++ plugins/functions/forcesub.py
@@ -15,7 +15,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/preetmodz).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/MrBot02).",
                 parse_mode=enums.ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_to_message_id=message.id,
